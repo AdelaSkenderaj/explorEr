003698589ebd94ca7c51ed0ceaeb9b3092cdd26e
AdelaSkenderaj <adelaskenderaj@gmail.com>
2023-04-27 23:07:24 +0200

    City CRUD

--- explorer-server/src/main/java/com/explorer/controller/CityController.java
+++ explorer-server/src/main/java/com/explorer/controller/CityController.java
@@ -1,12 +1,44 @@
 package com.explorer.controller;
 
+import com.explorer.entity.CityEntity;
 import com.explorer.service.CityService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequiredArgsConstructor
 public class CityController {
 
     private final CityService cityService;
+
+    @GetMapping("/cities")
+    public ResponseEntity<List<CityEntity>> getAllCitites() {
+        return ok(cityService.getAllCities());
+    }
+
+    @GetMapping("/cities/{id}")
+    public ResponseEntity<CityEntity> getCityById(@PathVariable Long id) {
+        return ok(cityService.getCityById(id));
+    }
+
+    @PostMapping("/cities")
+    public ResponseEntity<CityEntity> createCity(@RequestBody CityEntity cityEntity) {
+        return ok(cityService.createCity(cityEntity));
+    }
+
+    @PatchMapping("/citites/{id}")
+    public ResponseEntity<CityEntity> updateCity(@PathVariable Long id, @RequestBody CityEntity cityEntity) {
+        return ok(cityService.updateCity(id, cityEntity));
+    }
+
+    @DeleteMapping("/citites/{id}")
+    public ResponseEntity<Void> deleteCityById(@PathVariable Long id) {
+        cityService.deleteCityById(id);
+        return ok().body(null);
+    }
 }
--- explorer-server/src/main/java/com/explorer/service/CityService.java
+++ explorer-server/src/main/java/com/explorer/service/CityService.java
@@ -1,12 +1,58 @@
 package com.explorer.service;
 
+import com.explorer.entity.CityEntity;
+import com.explorer.entity.CountryEntity;
 import com.explorer.repository.CityRepository;
+import com.explorer.repository.CountryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Optional.ofNullable;
 
 @Service
 @RequiredArgsConstructor
 public class CityService {
 
     private final CityRepository cityRepository;
+    private final CountryRepository countryRepository;
+
+    public CityEntity findById(Long id) {
+        return cityRepository.findById(id).orElse(null);
+    }
+
+    public CityEntity getCityById(Long id) {
+        return findById(id);
+    }
+
+    public List<CityEntity> getAllCities() {
+        return cityRepository.findAll();
+    }
+
+    public CityEntity createCity(CityEntity cityEntity) {
+        return cityRepository.save(cityEntity);
+    }
+
+    public void deleteCityById(Long id) {
+        cityRepository.deleteById(id);
+    }
+
+    public CityEntity updateCity(Long id, CityEntity cityEntity) {
+        CityEntity ce = findById(id);
+        if (ce == null) {
+            throw new RuntimeException("City does not exist");
+        }
+        ofNullable(cityEntity.getName()).ifPresent(ce::setName);
+
+        Long countryId = cityEntity.getCountryId();
+        if (countryId != null) {
+            CountryEntity countryEntity = countryRepository.findById(countryId).orElse(null);
+            if (countryEntity == null) {
+                throw new RuntimeException("There does not exist a country with that id");
+            }
+            ce.setCountryId(countryId);
+        }
+        return cityRepository.save(ce);
+    }
 }
