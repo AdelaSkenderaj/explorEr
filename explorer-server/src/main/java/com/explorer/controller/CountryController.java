5600b3de8a4ee70129c3d30efbd0c7d92f1cefb8
AdelaSkenderaj <adelaskenderaj@gmail.com>
2023-04-27 22:45:01 +0200

    Country CRUD

--- explorer-server/src/main/java/com/explorer/controller/CountryController.java
+++ explorer-server/src/main/java/com/explorer/controller/CountryController.java
@@ -1,12 +1,45 @@
 package com.explorer.controller;
 
+import com.explorer.entity.CountryEntity;
 import com.explorer.service.CountryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RequiredArgsConstructor
 @RestController
 public class CountryController {
 
     private final CountryService countryService;
+
+    @GetMapping("/countries")
+    public ResponseEntity<List<CountryEntity>> getAllCountries() {
+        return ok(countryService.getAllCountries());
+    }
+
+    @GetMapping("/countries/{id}")
+    public ResponseEntity<CountryEntity> getCountryById(@PathVariable Long id) {
+        return ok(countryService.getCountryById(id));
+    }
+
+    @PostMapping("/countries")
+    public ResponseEntity<CountryEntity> createCountry(@RequestBody CountryEntity countryEntity) {
+        return ok(countryService.creatCountry(countryEntity));
+    }
+
+    @DeleteMapping("/countries/{id}")
+    public ResponseEntity<Void> deleteCountryById(@PathVariable Long id) {
+        countryService.deleteCountryById(id);
+        return ok().body(null);
+    }
+
+    @PatchMapping("/countries/{id}")
+    public ResponseEntity<CountryEntity> updateCountry(@PathVariable Long id, @RequestBody CountryEntity countryEntity) {
+        return ok(countryService.updateCountry(id, countryEntity));
+    }
+
 }
--- explorer-server/src/main/java/com/explorer/service/CountryService.java
+++ explorer-server/src/main/java/com/explorer/service/CountryService.java
@@ -1,12 +1,45 @@
 package com.explorer.service;
 
+import com.explorer.entity.CountryEntity;
 import com.explorer.repository.CountryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Optional.ofNullable;
 
 @Service
 @RequiredArgsConstructor
 public class CountryService {
 
     private final CountryRepository countryRepository;
+
+    public CountryEntity findById(Long id) {
+        return countryRepository.findById(id).orElse(null);
+    }
+    public List<CountryEntity> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    public CountryEntity getCountryById(Long countryId) {
+        return findById(countryId);
+    }
+
+    public CountryEntity creatCountry(CountryEntity countryEntity) {
+        return countryRepository.save(countryEntity);
+    }
+
+    public void deleteCountryById(Long countryId) {
+        countryRepository.deleteById(countryId);
+    }
+
+    public CountryEntity updateCountry(Long countryId, CountryEntity countryEntity) {
+        CountryEntity ce = findById(countryId);
+        if (ce == null) {
+            throw new RuntimeException("country does not exist");
+        }
+        ofNullable(countryEntity.getName()).ifPresent(ce::setName);
+        return countryRepository.save(ce);
+    }
 }
