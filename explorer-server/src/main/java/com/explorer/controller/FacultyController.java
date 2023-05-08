f6abf8c65b0762c82174145c1f6798be46130d8d
AdelaSkenderaj <adelaskenderaj@gmail.com>
2023-05-08 02:01:55 +0200

    Faculty CRUD

--- explorer-server/src/main/java/com/explorer/controller/FacultyController.java
+++ explorer-server/src/main/java/com/explorer/controller/FacultyController.java
@@ -1,12 +1,34 @@
 package com.explorer.controller;
 
+import com.explorer.entity.FacultyEntity;
+import com.explorer.entity.UniversityEntity;
 import com.explorer.service.FacultyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequiredArgsConstructor
 public class FacultyController {
 
     private final FacultyService facultyService;
+
+    @GetMapping("/faculty/{facultyId}")
+    public ResponseEntity<FacultyEntity> getFa(@PathVariable Long facultyId) {
+        return ok(facultyService.getFacultyById(facultyId));
+    }
+
+    @PostMapping("/faculty")
+    public ResponseEntity<FacultyEntity> createFaculty(@RequestBody FacultyEntity faculty){
+        return ok(facultyService.createFaculty(faculty));
+    }
+
+    @GetMapping("/university/{universityId}/faculties")
+    public ResponseEntity<List<FacultyEntity>> getAllUniversities(@PathVariable Long universityId) {
+        return ok(facultyService.getFacultiesByUniversityId(universityId));
+    }
 }
--- explorer-server/src/main/java/com/explorer/service/FacultyService.java
+++ explorer-server/src/main/java/com/explorer/service/FacultyService.java
@@ -1,12 +1,44 @@
 package com.explorer.service;
 
+import com.explorer.entity.FacultyEntity;
+import com.explorer.entity.UniversityEntity;
 import com.explorer.repository.FacultyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FacultyService {
 
     private final FacultyRepository facultyRepository;
+
+    public FacultyEntity findById(Long id) {
+        return facultyRepository.findById(id).orElse(null);
+    }
+
+    public List<FacultyEntity> getAllFaculties() {
+        return facultyRepository.findAll();
+    }
+
+    public List<FacultyEntity> getFacultiesByUniversityId(Long universityId) {
+        return facultyRepository.findByUniversityId(universityId);
+    }
+
+    public FacultyEntity createFaculty(FacultyEntity faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteUniversityById(Long id) {
+        facultyRepository.deleteById(id);
+    }
+
+    public FacultyEntity getFacultyById(Long facultyId) {
+        FacultyEntity faculty = findById(facultyId);
+        if (faculty == null) {
+            throw new RuntimeException("University not found");
+        }
+        return faculty;
+    }
 }
