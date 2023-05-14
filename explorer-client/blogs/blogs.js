500d6d05d96e85b5316ad0b37d43dd048a7ac866
Rigers Zaimi <rigersrigers123@gmail.com>
2023-05-14 13:49:11 +0200

    makingThePageDyanmic

--- explorer-client/blogs/blogs.js
+++ explorer-client/blogs/blogs.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    //Select all the buttons in the page
     const buttons = document.querySelectorAll("button");
-
+    
+    //Add an event listener for said buttons
     buttons.forEach(button => {
         button.addEventListener("click", getBlogIdExport)
         });
       });
 
-    
+    //This 
     function getBlogIdExport(){
         let blogId = parseInt(this.value)
         localStorage.setItem('blogId', blogId)
